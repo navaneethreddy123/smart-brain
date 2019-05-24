ef3e3ae825ac6ae7a2999cf9737ce3a02ffa2866
navaneethreddy123 <30905263+navaneethreddy123@users.noreply.github.com>
2019-05-23 23:50:11 -0400

    deploy

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://lit-ravine-11332.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
